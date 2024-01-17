e5b4d1c211bf2c1543df2467a413d3437a6b9265
Alissunka <sasha7122008@gmail.com>
2024-01-17 19:49:15 +0300

    gsffgh

--- game.py
+++ game.py
@@ -1,15 +1,12 @@
 import pygame
-from pygame.locals import *
+import os
 
 pygame.init()
+screen_size = (1200, 800)
+screen = pygame.display.set_mode(screen_size)
+FPS = 60
 
 clock = pygame.time.Clock()
-
-display_width = 800
-display_height = 600
-
-display = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('God of Caves')
 
 pygame.mixer.music.load("unki-shteker.mp3")
 pygame.mixer.music.set_volume(0.2)
@@ -17,8 +14,38 @@ pygame.mixer.music.set_volume(0.2)
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не удаётся загрузить:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+def start_screen():
+    fon = pygame.transform.scale(load_image('fon.jpg'), screen_size)
+    screen.blit(fon, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
 
 def run_game():
+    start_screen()
     pygame.mixer.music.play(-1)
     game = True
     while game:
@@ -31,13 +58,13 @@ def run_game():
         if keys[pygame.K_ESCAPE]:
             pause()
 
-        display.fill((255, 255, 255))
+        screen.fill((255, 255, 255))
         pygame.display.update()
 
-def print_pause(message, x, y, font_color = (0, 0, 0), font_type = 'Font_Pause.ttf', font_size = 36):
+def print_pause(message, x, y, font_color = (0, 0, 0), font_type = 'Font_Pause.ttf', font_size = 45):
     font_type = pygame.font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
-    display.blit(text, (x, y))
+    screen.blit(text, (x, y))
 
 def pause():
     paused = True
@@ -50,7 +77,7 @@ def pause():
                 pygame.quit()
                 quit()
 
-        print_pause('Пауза. Нажмите Enter', 270, 270)
+        print_pause('Пауза. Нажмите Enter.', 450, 370)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RETURN]:
